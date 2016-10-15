aa3129e69a3d340537834e7f7081e9b31b2c1ff5
woblavobla <soundtechproject@gmail.com>
2016-10-15 16:17:32 +0300

    Minor update

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_desired_films_session'
+Rails.application.config.session_store :cookie_store, key: '_fluffy_movies_session'
